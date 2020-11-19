f6e0a6adf28554b74a505fb9f7253e266fb2bd42
Reinsdyret <lars.haukland@gmail.com>
2020-11-19 10:05:35 +0100

    helo

--- gameOfLife/code.js
+++ gameOfLife/code.js
@@ -1,16 +1,38 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let preGenerations = document.getElementById("generations");
-let generationsPerSecond = document.getElementById("generationsPerSecond");
+let currentPopulation = document.getElementById("currentPopulation");
+let generationsPerSecond = document.getElementById("generationsPerSeconds");
 let preGenerationsPerSeconds = document.getElementById("generationsPerSecondsPre");
 let btnStart = document.getElementById("btnStart");
 
+let world, generations, interval;
 
-function population(arr){
+btnStart.onclick = function(){
+    generations = 0;
+    world = populate(25);
+    if(interval){
+        clearInterval(interval);
+    }
+    interval = setInterval(run, 1000 / generationsPerSecond.value);
+    preGenerationsPerSeconds.innerHTML = generationsPerSecond.value + " generations per second."
+}
+
+function run(){
+    currentPopulation.innerHTML = population(world);
+    preGenerations.innerHTML = generations;
+    drawWorld(world);
+    world = nextGeneration(world);
+
+    generations++;
+}
+
+function population(world){
+    //Recieves array of world and returns amount of cells that are alive(1).
     let num = 0;
-    for(let i = 0; i<arr.length; i++){
-        for(let j = 0; j<arr[i].length; j++){
-            if(arr[i][j] != 0){
+    for(let i = 0; i<world.length; i++){
+        for(let j = 0; j<world[i].length; j++){
+            if(world[i][j] != 0){
                 num++;
             }
         }
@@ -19,6 +41,7 @@ function population(arr){
 }
 
 function populate(size){
+    //Recieves size of array and returns array of randomly generated cells.
     let arr = new Array(size);
     let randInt;
     for(let i = 0; i<arr.length; i++){
@@ -27,7 +50,7 @@ function populate(size){
 
     for(let i = 0; i<arr.length; i++){
         for(let j = 0; j<arr[i].length; j++){
-            randInt = Math.floor(Math.random() * 1);
+            randInt = Math.floor(Math.random() * 2 - 1);
             arr[i][j] = randInt;
         }
     }
@@ -36,18 +59,18 @@ function populate(size){
 }
 
 function aliveNextGeneration(arr){
+    //Takes in 3x3 array with cell under investigation as arr[1][1]. Based on rules for survival it returns true or false.
     let populationAroundCenter = 0;
     for(let i = 0; i<arr.length; i++){
         for(let j = 0; j<arr[i].length; j++){
-            if(i != 2 && j != 2){
-                if(arr[i][j] == 1){
-                    populationAroundCenter++;
-                }
+            if(arr[i][j] == 1 && (i != 1 || j != 1)){
+                console.log(i,j);
+                populationAroundCenter++;
             }
         }
     }
 
-    if(populationAroundCenter == 2 && arr[2][2] == 1 || populationAroundCenter == 3){
+    if(populationAroundCenter == 2 && arr[1][1] == 1 || populationAroundCenter == 3){
         return true;
     }else{
         return false;
@@ -55,6 +78,7 @@ function aliveNextGeneration(arr){
 }
 
 function drawWorld(world){
+    //Draw rects based on world array
     ctx.clearRect(0,0,canvas.width,canvas.height);
     let blockSize = canvas.width / world.length;
 
@@ -65,4 +89,42 @@ function drawWorld(world){
             }
         }
     }
+}
+
+function nextGeneration(world){
+    //Take in current world and return next generation array.
+    let newWorld = new Array(world.length);
+    let cellBlock;
+    for(let i = 0; i<newWorld.length; i++){
+        newWorld[i] = new Array(newWorld.length);
+    }
+
+    for(let row = 0; row<world.length; row++){
+        for(let col = 0; col<world[row].length; col++){
+            cellBlock = [
+                [
+                world[(row + world.length + 1) % world.length, (col + world.length + 1) % world.length], 
+                world[(row + world.length + 1) % world.length, (col)],
+                world[(row + world.length + 1) % world.length, (col + 1) % world.length]
+                ],
+                [
+                world[row, (col + world.length + 1) % world.length],
+                world[row, col],
+                world[row, (col + 1) % world.length]
+                ],
+                [
+                world[(row + 1) % world.length, (col + world.length + 1) % world.length],
+                world[(row + 1) % world.length, col],
+                world[(row + 1) % world.length, (col + 1) % world.length]
+                ]
+            ];
+            if(aliveNextGeneration(cellBlock)){
+                newWorld[row,col] = 1;
+            }else{
+                newWorld[row,col] = 0;
+            }
+        }
+    }
+
+    return newWorld;
 }
